8bfef2145dea4ef3bb93b5c13695b4c0814e1c55
twahanur <thohanur143@gmail.com>
2025-07-16 22:06:19 +0600

    readme updated

--- context/AuthContext.jsx
+++ context/AuthContext.jsx
@@ -79,7 +79,6 @@ export const AuthProvider = ({ children }) => {
 
     const login = async (state, credentials) => {
       try {
-
         const {data} = await axios.post(`/api/auth/${state}`,credentials);
         if(data.success){
           setAuthUser(data.userData);
